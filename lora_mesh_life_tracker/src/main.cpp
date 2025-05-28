afccab6af01918c400d8d26dafd5ddcb14092184
GLeidius <gleidar5000@gmail.com>
2025-05-28 13:18:22 +0700

    1.4.5 добавлено дублирование пакета на сервер, при отправке в меш

--- lora_mesh_life_tracker/src/main.cpp
+++ lora_mesh_life_tracker/src/main.cpp
@@ -15,6 +15,7 @@ void loop()
   int butt_count = 1;
   int status_count = 1;
   int power_counter = 22;
+  int TX_timeout_random_piece = 0;
   bool connect_flag = 0;
   String Module_ADDR = Set_E52_ADDR();                       // устанавливаем и запоминаем адрес Е52
   Display_coordinates cord = init_menu(Module_ADDR);
@@ -31,9 +32,14 @@ void loop()
       get_setup_from_ESP();                                 // получаем строку с настройками инастраиваемся
     }
 
-      if ((millis() - start_time) >= 5000)                  // режим отправки и отправка
+      if ((millis() - start_time) >= 3000+TX_timeout_random_piece)                  // режим отправки и отправка
     {
       start_time = millis();
+      /*
+      MySerial1.print("TX_timeout=");
+      MySerial1.println(10000+TX_timeout_random_piece);
+      */
+      TX_timeout_random_piece = random(0,2000);
       MySerial1.println("===========================================================================================");
 
       String data_transmitt = get_telemetry(Module_ADDR, status_count, altitude_rate);
@@ -42,6 +48,17 @@ void loop()
       { 
         draw_pos(cord.Mode_Xpos, cord.Mode_Ypos, "Mesh");
         send_to_mesh_E52(data_transmitt); // отправляем пакет (Если что то с модулем: data_transmitt = "GL 6666 56.452051 84.962577 174.967 1.5 190.4 1 2";)
+      
+        if (connect_flag == 0)                  // добавил дублирование отправкой на сервер, по просьбе организатора
+        { 
+          try_connect_to_server();                            // пытаемя подключиьтся к серверу
+          connect_flag = check_connect_to_server();           // проверяем получилось ли подключиться 
+        }
+
+        if (connect_flag == 1)            // !!!!!!!!!0 потому что хочу посмотреть что пишет в таком случае должен быть 1!!!!!!!!
+        {
+          connect_flag = send_to_server_SIM868(data_transmitt);              // если получилось подключиться то отправляем данные
+        }
       }
 
       else if (digitalRead(STM_SW6) == true)                    // INTERNET 
@@ -66,12 +83,12 @@ void loop()
       butt_count = Next_SR(butt_count, cord.SR_Xpos, cord.SR_Ypos);
     }
     
-    if (digitalRead(STM_RT) == false)                                                 //  STATUS  
+    if (digitalRead(STM_RT) == false)                                                  //  STATUS  
     { 
       status_count = Next_status(status_count, cord.Stat_Xpos, cord.Stat_Ypos);
     }
     
-    if (digitalRead(STM_OK) == false)                                                   //  POWER 
+    if (digitalRead(STM_OK) == false)                                                  //  POWER 
     {  
       power_counter = Next_power(power_counter, cord.Power_Xpos, cord.Power_Ypos);
     }
