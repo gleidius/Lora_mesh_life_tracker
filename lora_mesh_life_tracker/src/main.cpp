c90a5aad64e5753070876d53da577a972cec88a1
GLeidius <gleidar5000@gmail.com>
2025-07-17 14:31:28 +0700

    only SIM power ON

--- lora_mesh_life_tracker/src/main.cpp
+++ lora_mesh_life_tracker/src/main.cpp
@@ -8,7 +8,7 @@
 
 // #define MESH_STATUS_PIN PB7
 
-uint8_t MESH_STATUS_PIN = PB5;
+uint8_t MESH_STATUS_PIN = PB9;
 
 SIM868 sim868(Terminal_UART, SIM868_UART);
 Screen my_screen(Terminal_UART);
@@ -24,17 +24,22 @@ void setup()
 	my_screen.drawBitmap(0, 0, PODNEBESE_LOGO, 128, 64, SSD1306_WHITE);
 
 	sim868.power_ON(SIM_PWRK);
-	sim868.PowerUp_gps();
+	// sim868.PowerUp_gps();
+	// delay(2000);
+	// sim868.send_AT_command("AT+CGNSCMD=0,\"$PMTK353,1,1,1,0,0*2A\"");
+
 	my_screen.fillRect(0, 0, 128, 64, SSD1306_BLACK);
 	my_screen.display();
-	pinMode(MESH_STATUS_PIN, INPUT);
+	pinMode(MESH_STATUS_PIN, INPUT_PULLDOWN);
 }
 bool state_MESH;
 
 void loop()
 {
-	Terminal_UART.println("loop");
+	// Terminal_UART.println("loop");
 	state_MESH = digitalRead(MESH_STATUS_PIN);
+
+	Terminal_UART.print(state_MESH);
 	Terminal_UART.println(state_MESH);
 
 	if (state_MESH == 0)
